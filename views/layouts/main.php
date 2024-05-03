db6f4671d3a87867154c23359b9b717eb5e5b1db
Yehor Rykhnov <egor.developer@gmail.com>
2024-05-03 12:55:57 +0300

    Add favicon

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,6 +26,10 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
 </head>
 <body class="d-flex flex-column h-100">
 <?php $this->beginBody() ?>
